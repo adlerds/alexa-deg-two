c89aaaf80448a89818a0a6b52883d6aa2b424a9f
adlerds <david@dadigital.com>
2018-05-08 11:44:42 -0500

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -91,7 +91,7 @@ exports.Changes = (slots, session, response) => {
                 if (priceChanges && priceChanges.length>0) {
                     let text = `OK, your order is expected delivery on `;
                     priceChanges.forEach(property => {
-                        text += `${property.get("Delivery_date__c")}. <break time="0.5s" /> `;
+                        text += `${property.get("emails_sent__C")}. <break time="0.5s" /> `;
                     });
                     response.say(text);
                 } else {
